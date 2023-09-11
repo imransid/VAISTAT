1f720302cd87b9cbb8d4f19ee9475e4053106bd4
Imran Khan <emailofimran1992@gmail.com>
2023-09-11 20:15:40 +0600

    VAISTAT-03: SignIN UI

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,6 @@
 import Logo from './Icons/Logo';
+import FBIcon from './Icons/FBIcon';
+import Google from './Icons/GoogleIcon';
+import LockUser from './Icons/UserIcon';
 
-export { Logo };
+export { Logo, FBIcon, Google, LockUser };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
+import Button from './Button';
 import Checkbox from './CheckBox';
 import { CustomDrawer } from './CustomDrawer';
 import { CustomTabBarButton } from './CustomTabBarButton';
 import TextInput from './TextInput';
 import TextItem from './TextItem';
-export { Checkbox, CustomDrawer, CustomTabBarButton, TextInput, TextItem };
+export { Button, Checkbox, CustomDrawer, CustomTabBarButton, TextInput, TextItem };
